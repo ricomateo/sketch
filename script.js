d16fecb162d9d25f5feda6b3e559161856e81cf6
ricomateo <mateorico2001@gmail.com>
2022-08-12 18:51:30 -0300

    Add a function to resize the grid using a button

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-function createGrid() {
+function createGrid(numberOfSquares) {
     let divContainer = document.querySelector('.container');
-    for (let i = 0; i < 256; ++i) {
+    for (let i = 0; i < numberOfSquares ** 2; ++i) {
         let div = document.createElement('div');
         div.className = 'square';
+        let newHeight = Math.floor(512 / numberOfSquares);
+        div.style.height = `${newHeight}px`;
+        div.style.width = `${newHeight}px`;
         divContainer.appendChild(div);
     }
 }
 
-createGrid();
+
+function sketch() {
+    let numberOfSquares = 16;
+    createGrid(numberOfSquares);
+    let sizeButton = document.querySelector('.size-button');
+    sizeButton.addEventListener('click', resizeGrid);
+}
+
+function removeGrid() {
+    let divContainer = document.querySelector('.container');
+    let squares = document.querySelectorAll('.square');
+    for (const square of squares) {
+        divContainer.removeChild(square);
+    }
+}
+
+function resizeGrid() {
+    numberOfSquares = prompt("Enter the number of squares: ", "");
+    removeGrid();
+    createGrid(numberOfSquares);
+}
+
+sketch();
